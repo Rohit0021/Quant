8beeb94305c050a36018a9aa71076f383d80aff9
Rohit0021 <rohitdhakad097@gmail.com>
2026-08-13 16:49:21 +0530

    update

--- Back/index.js
+++ Back/index.js
@@ -1,18 +1,75 @@
 const { spawn } = require('child_process');
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+
 
 const argv = process.argv;
 const args = parseArgs(argv);
 
 switch (argv[2]) {
   case "backtest": {
-    if (!args.algo /*|| !args.data || !args.out*/ ) showUseExit();
+    if (!args.algo || !args.data || !args.out || !args.name) showUseExit();
     
     dotnetBuild(args.algo);
+    
     break;
   }
   
   default:
     showUseExit();
+}
+
+function temp(param) {
+  
+  const dt = new Date();
+  const outdir = args.out + `/${dt / 1}::${dt.toString()}`;
+  console.log(outdir);
+  
+  const data = {
+    "algorithm-type-name": "EMACross",
+    "algorithm-language": "CSharp",
+    "algorithm-location": `${args.algo}/bin/Debug/${args.name}.dll`,
+    
+    "results-destination-folder": outdir,
+    
+    "data-folder": args.data,
+    
+    "live-mode": false,
+    
+    "messaging-handler": "QuantConnect.Messaging.Messaging",
+    "job-queue-handler": "QuantConnect.Queues.JobQueue",
+    "api-handler": "QuantConnect.Api.Api",
+    
+    "setup-handler": "QuantConnect.Lean.Engine.Setup.ConsoleSetupHandler",
+    "result-handler": "QuantConnect.Lean.Engine.Results.BacktestingResultHandler",
+    "data-feed-handler": "QuantConnect.Lean.Engine.DataFeeds.FileSystemDataFeed",
+    "real-time-handler": "QuantConnect.Lean.Engine.RealTime.BacktestingRealTimeHandler",
+    "transaction-handler": "QuantConnect.Lean.Engine.TransactionHandlers.BacktestingTransactionHandler"
+  };
+  
+  const tempFile = path.join(
+    os.tmpdir(),
+    `my-data-${Date.now()}-${process.pid}.json`
+  );
+  
+  fs.writeFileSync(tempFile, JSON.stringify(data, null, 2));
+  
+  console.log(tempFile);
+  
+  /////////////
+  
+  const child = spawn('dotnet', ['/root/Quant/Lean/Launcher/bin/Debug/QuantConnect.Lean.Launcher.dll', "--config", `${tempFile}`], {
+    stdio: 'inherit',
+    shell: false
+  });
+  
+  child.on('close', (code) => {
+    if (code !== 0) {
+      console.error(`dotnet build failed with exit code ${code}`);
+      process.exitCode = code;
+    }
+  });
 }
 
 function dotnetBuild(projectPath) {
--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,5 @@
 # gh repo create Quant --public --source=. --remote=origin --push
 
 cd Back
-node index.js backtest --algo=/root/Quant/Algo
+node index.js backtest --algo=/root/Quant/Algo --name=Algo --data=/root/Quant/Static/data --out=/root/Quant/Algo/backtest
+
